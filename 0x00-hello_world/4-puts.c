30c82b2d11390460e3c648ceafe6b9da72016ba5
LoisAfia <lagbemvai-kudufia@aims.edu.gh>
2023-10-05 16:49:31 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <stdio.>
+#include <stdio.h>
 /**
  * main - A program that prints a line with puts function
  * Return: 0 (Success)
@@ -9,4 +9,3 @@ puts("\"Programming is like building a multilingual puzzle
 ");
 return (0);
 }
-
