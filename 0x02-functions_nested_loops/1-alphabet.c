816642e2bc70f28f95451f470061f4cccc94dd90
LoisAfia <lagbemvai-kudufia@aims.edu.gh>
2023-10-10 11:01:12 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <main.h>
+
+/**
+ * print_alphabts - Make the alphabet
+ * Return: void
+ */
+
+void print_alphabe(void)
+{
+	char c;
+
+	for (c = 'a'; c<= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
+
