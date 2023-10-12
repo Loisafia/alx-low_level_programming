5246b1866797ff2206df4d37b34b18451f1d2cac
LoisAfia <lagbemvai-kudufia@aims.edu.gh>
2023-10-12 16:51:30 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,8 +11,8 @@ void print_most_numbers(void)
 
 	for (i = 0 ; i < 10 ; i++)
 	{
-		if (i != 2 && 1 != 4)
-			putchar(1 + '0');
+		if (i != 2 && i != 4)
+			putchar(i + '0');
 	}
 	putchar('\n');
 }
