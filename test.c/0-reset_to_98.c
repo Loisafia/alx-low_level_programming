d42615a28232172e56df496770aefa1d898633f3
LoisAfia <lagbemvai-kudufia@aims.edu.gh>
2023-10-17 04:53:06 -0700

    test 1

---
+++ test.c/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#inlcude "main.h"
+
+/**
+ * reset_to_98 - a function that takes a pointer to an int as parameter
+ * and updates the value to points to to 98
+ * @n: input
+ * Return; n
+ */
+
+void reset_to_98(int*n)
+{
+	*n = 98;
+}
---
+++ test.c/main.h
@@ -0,0 +1,9 @@
+#idendf MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char);
+
+#endif
